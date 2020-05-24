d399b2fef6c1c9290dfa1a0ebf9ffc81c6dd6d8a
Shashwa7 <55311355+Shashwa7@users.noreply.github.com>
2020-05-24 17:44:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-//Select element function
-const selectElement = element => {
-    return document.querySelector(element);
-};
+const nav_slide = () => {
 
-const menuToggler = selectElement('.menu-toggle');
-const body = selectElement('body');
-const nav_list = selectElement('.nav-list');
-const title = selectElement('#main-topic');
+    const navList = document.querySelector('.nav-list');
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelectorAll('.nav-items');
 
-menuToggler.addEventListener('click', () => {
-    body.classList.toggle('open');
+
+    burger.addEventListener('click', () => {
+        navList.classList.toggle('nav-active');
+    
+        //animate links
+        navLinks.forEach((link, index) => {
+             
+            //for this animation to occur again and again
+            if(link.style.animation){
+                link.style.animation = ''; //if already present then no need
+            }
+            else{
+                link.style.animation = `nav-fade .5s ease-in forwards ${index / 7 }s`;//delay for each nav link
+            }
+            
+        });
+
+        burger.classList.toggle('toggle');
+
 });
 
-nav_list.addEventListener('click', () => {
-    body.classList.toggle('open');
-});
+}
 
-title.addEventListener('click', () => {
-    body.classList.toggle('open');
-});
+nav_slide();
